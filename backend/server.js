9e2de24b289a108bebd663f7a86987c24c870b6b
Gautam Bansal <gautam.bansal@toddleapp.com>
2024-08-18 21:41:17 +0530

    added frontend url to env

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: process.env.FRONTEND_URL }));
 
 app.get("/", async (req, res) => {
   return res.send("<h1>Welcome to Solana Alerts API</h1>");
